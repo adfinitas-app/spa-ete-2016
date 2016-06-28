e05bbcf32aa8db0516f06c96fcf274b648ce27d3
lghionna <lghionna@adfinitas.fr>
2016-06-28 12:35:32 +0200

    change data-cta.js

--- js/data-cta.js
+++ js/data-cta.js
@@ -1,16 +1,16 @@
 var _DATA_CTA = {
   // Peut-être un tableau ou un nombre
   // L'utilisation de plusieurs cid est utile en cas d'AB test
-  iraiser_cid : 53,
-  iraiser_url : 'don.spa.asso.fr',
+  iraiser_cid : 115,
+  iraiser_url : 'soutenir.la-spa.fr',
 
   // Nom de la catégorie de l'evenement remonté dans Woopra
   // Doit être une string (le numéro du bouton CTA y sera concaténé)
-  woopra_interaction :   'don-IMG_ete2015',
+  woopra_interaction :   'don-CTA_eliot16',
 
   // Upgrade affiché sur iRaiser
   segments : {
-    'OPTIN':    {'arrondiSup':0, 'type': 'fixe',     'values':[20, 30, 50]} ,
+    'OPTIN':    {'arrondiSup':0, 'type': 'fixe',     'values':[35, 55, 75, 120]} ,
     'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0, 5, 10]} ,
     'DONATEUR': {'arrondiSup':5, 'type': 'percent',  'values':[0, 25, 50]} ,
     'MIDDLE':   {'arrondiSup':5, 'type': 'addition', 'values':[0, 25, 100]} ,
